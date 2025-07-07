2b20c66d020c01ee33b12a0bf2e4b03c051c4829
Kalyan Bhagavan <kalyanbhagavanit@gmail.com>
2025-07-07 21:54:14 +0200

    update

--- modules/keyvault/main.tf
+++ modules/keyvault/main.tf
@@ -75,3 +75,8 @@ resource "azurerm_key_vault_secret" "github_runner_token" {
     ignore_changes = [value]
   }
 }
+
+output "key_vault_id" {
+  description = "Key Vault resource ID."
+  value       = azurerm_key_vault.main.id
+}
